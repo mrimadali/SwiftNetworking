d4581b1655c92b30cf2acd992477d655e2af7f1a
Mohammad Imad Ali <mrimadali@gmail.com>
2024-08-08 18:48:50 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftNetworking",
-            path: "Sources"
+            path: "Sources/SwiftNetworking"
         ),
         .testTarget(
             name: "SwiftNetworkingTests",
