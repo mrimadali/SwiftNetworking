25abae06a4870ea90aa527648664c4b56b433617
Imad Ali <mrimadali@gmail.com>
2024-08-10 17:45:23 +0530

    Updated folder

--- SwiftNetworking/Package.swift
+++ SwiftNetworking/Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftNetworking",
-            path: "Sources/SwiftNetworking"
+            path: "Sources"
         ),
         .testTarget(
             name: "SwiftNetworkingTests",
