7a9b76b04eebf4f43e5a65d4d723621f82725972
Imad Ali <mrimadali@gmail.com>
2024-08-10 17:30:55 +0530

    Folder structure updates

---
+++ SwiftNetworking/SwiftNetworking/Sources/SwiftNetworking/APIConfiguration.swift
@@ -0,0 +1,45 @@
+//
+//  APIConfiguration.swift
+//  
+//
+//  Created by Imaad  on 8/8/24.
+//
+
+import Foundation
+
+// Template to construct API endpoints
+protocol APIConfiguration {
+    var urlPath: String { get }
+    var parameters: [URLQueryItem] { get }
+    var method: APIMethod { get }
+    var headers: [String: String]? { get }
+    var body: Encodable?{ get }
+}
+
+extension APIConfiguration {
+    var isAuthenticated: Bool {
+        if let accessToken = headers?["Authorization"], accessToken.count > 0 {
+            return true
+        }
+        return false
+    }
+}
+
+enum APIMethod {
+    case get, post, patch, put, delete
+    
+    var description: String {
+        switch self {
+        case .get:
+            return "GET"
+        case .post:
+            return "POST"
+        case .patch:
+            return "PATCH"
+        case .put:
+            return "PUT"
+        case .delete:
+            return "DELETE"
+        }
+    }
+}
